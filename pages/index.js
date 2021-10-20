bcc2df982209c60ef1319ef3c494f3bdf216bf74
danmurciano <60635039+danmurciano@users.noreply.github.com>
2021-10-19 21:05:40 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,24 +9,13 @@ import weatherResponse from "../utils/weatherResponse";
 export default function Home() {
   const [searchState, setSearchState] = React.useState("");
   let search = searchState;
-  const [location, setLocation] = useState("Nothing yet");
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
-  const [weatherData, setWeatherData] = useState(null);
-  let now = weatherData;
-  const [weather, setWeather] = useState(null);
-
-
   const router = useRouter();
-  const [success, setSuccess] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
-  const [disabled, setDisabled] = React.useState(true);
-  const [error, setError] = React.useState("");
 
 
   function handleChange(event) {
     setSearchState(event.target.value);
   }
+
 
   function handleClearSearch(event) {
     search = "";
@@ -40,9 +29,8 @@ export default function Home() {
   }
 
 
-
   return (
-    <div class="page-main page-index">
+    <div class="page-index">
       <Header/>
       <div class="container-fluid home-image" style={{ backgroundImage: `url(/images/banner_back.jpg)` }}>
         <div>
